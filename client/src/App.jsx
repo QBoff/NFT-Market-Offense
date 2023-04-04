6b78ed7d8444660d8c6aa23f9fa5c9026697085d
Dany <86149558+QBoff@users.noreply.github.com>
2023-04-04 22:25:53 +0300

    Создал шаблон на react
    -> сделал заготовку для navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Navbar } from './Components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
