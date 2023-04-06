57dc8152404a2fb7ac9d9b4e6d26a3927b63c8bf
Andrew <andrey777-novikov777@yandex.ru>
2023-04-06 07:53:18 +0300

    Разбиение дизайна на файлы и небольшая стилизация

--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,40 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 assert load_dotenv(), "Даня, ты забыл .env добавить"
 
 app = Flask(
     import_name="NFT-Market-Offense",
-    static_folder=os.path.join("client", "static")
+    static_folder=os.path.join("client", "static"),
+    template_folder=os.path.join("client", "templates")
 )
 app.secret_key = os.getenv("WEBSITE_SECRET_KEY_AA01")
 
 
 @app.route("/")
-def index():
-    return "<body><h2>Даня пока ничего не сделал =)</h2></body>"
+@app.route("/home")
+def home():
+    return render_template("base.html")
+
+
+@app.route("/market")
+def market():
+    return render_template("base.html")
+
+
+@app.route("/create")
+def nft_creation():
+    return render_template("base.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("base.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
